6093d9ede3c16911798a4a4582c89fa4d1d3a9ab
Raphael Leu <raphleu@gmail.com>
2020-03-13 15:14:02 -0400

    silly string vs int comparison mistkae

--- server_scripts/fix_zone_size.py
+++ server_scripts/fix_zone_size.py
@@ -2,16 +2,16 @@
 
 while True:
     line = input()
-    # triggering off the NUM_HUMANS line which seems to be outputted at the start of every round. Unfortunately, this 
+    # triggering off the NUM_HUMANS line which seems to be outputted at the start of every round. Unfortunately, this
     # might mean size adjustments are delayed by 1 round
     if line.startswith('NUM_HUMANS'):
         parsed_line = line.split()
         # make sure num_players has a second argument passed to it
         if (parsed_line[1]):
             num_players = parsed_line[1]
-            if (num_players == 2):
-                print ('console_message Adjusting zone size for 2 players\n')
+            if (num_players == '2'):
+                print ('console_message Adjusting zone size for 2 players next round\n')
                 print ('SIZE_FACTOR -1\n')
-            elif (num_players == 3):
-                print ('console_message Adjusting zone size for 3 players\n')
+            elif (num_players == '3'):
+                print ('console_message Adjusting zone size for 3 players next round\n')
                 print ('SIZE_FACTOR 0\n')
